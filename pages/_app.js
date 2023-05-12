6a3eac8854e7f9fa7115cbbd143ae9c24480e7ce
Ekweaga <hikay133@gmail.com>
2023-05-12 16:12:23 +0100

    GrahQl Apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const client = new ApolloClient({
+    uri: 'https://graphql.anilist.co/',
+    cache: new InMemoryCache(),
+  });
+  return (
+    <ApolloProvider client={client}>
+ <Component {...pageProps} />
+
+    </ApolloProvider>
+  )
+  
+ 
 }
